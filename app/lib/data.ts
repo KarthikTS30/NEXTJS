5feb840c0fdb90734e6f83961b30165f2b755ff8
KarthikTSK <karthikts@claritytechlabs.com>
2026-09-12 22:06:20 +0530

    update dynamic for rendering queries

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,6 +9,8 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
+export const dynamic = 'force-dynamic';
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
@@ -17,7 +19,6 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
@@ -32,8 +33,7 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    console.log('Fetching latest invoices...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching latest invoices...');    
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
